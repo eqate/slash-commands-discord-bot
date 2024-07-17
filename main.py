024fcf73b992d943989e0ef0bdb71a1844800aee
eqate <166958338+eqate@users.noreply.github.com>
2024-07-16 22:29:08 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+import discord
+from discord import app_commands
+from discord.ext import commands
+from config import TOKEN
+
+bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
+
+@bot.event
+async def on_ready():
+    print("Bot is alive")
+    try:
+        synced = await bot.tree.sync()
+        print(f"Synced {len(synced)} command(s)")
+    except Exception as e:
+        print(e)
+@bot.tree.command(name="test")
+async def hello(interaction: discord.Interaction):
+    await interaction.response.send_message(f"i'm online!", ephemeral=True) # ephmeral = hidden/personal message
+@bot.tree.command(name="say")
+@app_commands.describe(arg="What should I say?")
+async def say(interaction: discord.Interaction, arg: str):
+    await interaction.response.send_message(f"{interaction.user.name} said: `{arg}`") # do {arg} only if you want it to just repeat what oyu said.
+
+bot.run(TOKEN)
